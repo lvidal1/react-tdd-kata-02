4965443c5022034166b1f102e538cfc15afe465d
lvidal1 <lvida910@gmail.com>
2022-01-16 15:31:00 -0500

    Added redux store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import {Provider} from 'react-redux';
+import store from './store';
+
 import {RestaurantScreen} from './components/RestaurantScreen';
 
 const App = () => {
     return (
-        <div>
+        <Provider store={store}>
             <RestaurantScreen />
-        </div>
+        </Provider>
     );
 };
 
